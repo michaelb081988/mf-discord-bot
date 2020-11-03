ff7e72c4be5baf2bc2d41d4ad794cdf5d62065fb
Michael <michaelb081988@gmail.com>
2020-11-03 17:06:18 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,20 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
- 
+
+const squad = () => new Discord.MessageEmbed()
+      .setAuthor('TOTO', "https://i.imgur.com/ezC66kZ.png")
+      .setColor('#AAA')
+      .setTitle('First')
+      .setDescription('First');
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
  
 client.on('message', message => {
-    if (message.content === 'ping') {
-       message.reply('pong');
-       }
+    if (message.content === '!squad') {
+       message.channel.send(first);
+    }
 });
  
 // THIS  MUST  BE  THIS  WAY
