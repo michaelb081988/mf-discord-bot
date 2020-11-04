b89a7ae0718f25e61ee91eabefa91968461daf16
Michael <michaelb081988@gmail.com>
2020-11-04 12:42:30 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,24 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const exampleEmbed = new Discord.MessageEmbed()
-	.setColor('#0099ff')
-	.setTitle('Some title')
-	.setURL('https://discord.js.org/')
-	.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-	.setDescription('Some description here')
-	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
-	.addFields(
-		{ name: 'Regular field title', value: 'Some value here' },
-		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-	)
-	.addField('Inline field title', 'Some value here', true)
-	.setImage('https://i.imgur.com/wSTFkRM.png')
-	.setTimestamp()
-	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
-
 client.on('ready', () => {
     console.log('I am ready!');
 });
@@ -27,7 +9,6 @@ client.on('message', message => {
     let color = 7081235; // hex: #6C0D13
     
     if (message.content === '!squad') {
-        message.channel.send(exampleEmbed);
     }
 });
  
