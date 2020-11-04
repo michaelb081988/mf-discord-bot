8d7fe41aeee2ac771b510f12afebca83fcf99274
Michael <michaelb081988@gmail.com>
2020-11-04 15:34:15 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+let members = [];
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
@@ -9,6 +11,19 @@ client.on('message', message => {
     let color = 7081235; // hex: #6C0D13
     
     if (message.content === '!squad') {
+        members.push("Test");
+    }
+    
+    if(message.content === '!start') {
+        let message = "Joining members: ";
+        members.forEach(function(item, index, array) {
+            message = message + item;
+        }));
+        message.channel.send(message);
+    }
+    
+    if(message.content === '!clear') {
+        members = [];
     }
 });
  
