6cf97b7edccd119516e68c8a5d95286a16ce4162
Michael <michaelb081988@gmail.com>
2021-03-03 13:25:59 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,12 @@ const client = new Discord.Client();
 process.env.TZ = 'Australia/Perth'
 
 let messaged = false;
+let lastHours = 0;
+let lastMinutes = 0;
 
 //Event Timers // Setting them to FALSE turns them off completely...
-let bloodEvent = true; // Guild Box O' Grimoire // AKA Trash Event
+let bloodEvent = false; // Guild Box O' Grimoire // AKA Trash Event
+
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -19,44 +22,66 @@ client.on('message', message => {
     }
     
     if (message.content === '!blood') {
-        client.channels.get('762832294259195946').send('Please don\'t forget to spend your Blood! https://i.imgur.com/HKw7PQj.jpg');
+        if(bloodEvent) {
+            bloodEvent = false;
+            message.reply(" Blood event is disabled!");
+        } else {
+            bloodEvent = true;
+            message.reply(" Blood event enabled! Have fun! https://i.imgur.com/HKw7PQj.jpg");
+        }
     }
 });
-
 
 function doStuff() {
     let date = new Date();
     
+    // Mani test event to the spammy level chat
+        if(CheckTime(13, 30)) {
+            if(!messaged) {
+                client.channels.get('763085331565117482').send('New time test with a bunch of other stuff... https://i.imgur.com/DehsKa7.jpg');
+                messaged = true;
+            }
+    }
+    
     // Colo alert timer
-    if(date.getHours() === 9) {
-        if(date.getMinutes() == 57) {
+    if(CheckTime(9, 57)) {
             if(!messaged) {
                 client.channels.get('762832294259195946').send('@everyone Time for Colo! https://i.imgur.com/DehsKa7.jpg');
                 messaged = true;
             }
-        }
-        if(date.getMinutes() == 58) {
-            messaged = false
-        }
     }
     
     // Blood Event
     if(bloodEvent) {
         //Right after our colo ends + a minute or 2
-        if(date.getHours() === 10) {
-            if(date.getMinutes() == 22) {
+        if(CheckTime(10, 22)) {
                 if(!messaged) {
                     client.channels.get('762832294259195946').send('Please don\'t forget to spend your Blood! https://i.imgur.com/HKw7PQj.jpg');
                     messaged = true;
                 }
-            }
-            if(date.getMinutes() == 23) {
-                messaged = false
-            }
+        }
+    }
+    
+    //Reset sent message one minute later to stop spam because i am lazy to do this properly
+    if(messaged) {
+        if(CheckTime(hours, minutes +1)) {
+           messaged = false;
+           lastHours = hours-1;
         }
     }
 }
 let myTimer = setInterval(doStuff, 1000); //time is in ms
+
+function CheckTime(hours, minutes) {
+    if(date.getHours() === hours) {
+        if(date.getMinutes() === minutes) {
+            lastHours = hours;
+            lastMinutes = minutes;
+            return true;
+        }
+    }
+    return false;
+}
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
