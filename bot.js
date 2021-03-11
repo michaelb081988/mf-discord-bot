6f1a36293e76d84a5f5b7d5ba4b9706681a342a6
Michael <michaelb081988@gmail.com>
2021-03-12 00:51:33 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -147,7 +147,7 @@ cron.schedule('0 21 10 * * *', () => {
 // Check 
 cron.schedule('0 * * * * *', () => {
     if(guerrillaEvent) {
-        if(isTime(2, 28) || isTime(4, 28) || isTime(6, 28) || isTime(8, 28) || isTime(10, 28) || isTime(18, 28)) {
+        if(isTime(2, 28) || isTime(4, 28) || isTime(6, 28) || isTime(8, 28) || isTime(10, 28) || isTime(18, 28) || isTime(0,30) {
             sendEvent(eventChannel, "Time to farm up some fun! For the next 30 minutes the Weapon/Armor Upgrade Materials events are running.", "https://static.wikia.nocookie.net/sinoalice_gamepedia_en/images/7/71/Guerrilla_weapon.png");
         }
     }
