4b92c4614c15028973cbc7be7e5068dc833256f2
Michael <michaelb081988@gmail.com>
2021-03-11 14:18:00 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -223,7 +223,7 @@ function doesGuildExist(guild) {
 
 function isEventActive(event) {
     db
-    .query("SELECT * FROM EVENTS WHERE name = '" + event + "'")
+    .query("SELECT * FROM EVENTS")
     .then(res => { return JSON.stringify(res.rows) });
 }
 
