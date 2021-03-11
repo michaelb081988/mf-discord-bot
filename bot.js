270f39960b0d367726f023752a2967a3330f2b64
Michael <michaelb081988@gmail.com>
2021-03-11 14:50:55 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -224,7 +224,7 @@ function doesGuildExist(guild) {
 function isEventActive(event) {
     db
     .query("SELECT * FROM EVENTS")
-    .then(res => { return JSON.stringify(res.rows) });
+    .then(res => { res.rows[0]['active'] });
 }
 
 // This connects the bot to the Discord servers, without this nothing starts
